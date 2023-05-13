4a235e808b98e07d6928809e6b17985e8410868e
LiWanHou <quannhan6969@gmail.com>
2023-05-13 18:04:00 +0700

    a

---
+++ main.js
@@ -0,0 +1,11 @@
+function Validator(options){
+    const formElement = document.querySelector(options.form)
+    console.log(formElement)
+}
+
+Validator.isRequiredFullname = function(){
+
+}
+Validator.isRequiredEmail = function(){
+
+}
